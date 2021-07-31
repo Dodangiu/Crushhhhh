025ed3789d2e809695a1a14d077caa485879395e
Dodangiu <88225211+Dodangiu@users.noreply.github.com>
2021-07-31 22:40:08 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Heluuuuu cậu dth!',
+    introDesc: `Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhoaaaa!`,
+    btnIntro: 'Okieee',
+    title: 'Cậu yêu tớ có phải hong nàoooo ._.',
+    desc: 'Nếu cậu hong trả lời mà thoát ra tức là cậu iu tớ đấy nháaa :3',
+    btnYes: 'Iu ơi là iuuuu <33',
+    btnNo: 'Cậu mơ àaa :v',
+    question:'Nói cho tớ biết lí do cậu iu tớ đii ^^',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Cậu xink thế kia hong iu thì phí :>>>>>>>>>>>>',
+    mess: 'Tớ biết mà ^^ Love u 3 thousand  nàa',
+    messDesc: 'Tối nay rảnh thì videocall với tớ nhaaa :3 Còn chần chừ gì nữa mà ko ib tớ đi nàoooo',
+    btnAccept: 'Okiiiii lunn <3',
+    messLink: 'http://m.me/100070504141368'
